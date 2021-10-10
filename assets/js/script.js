d119ea070da1a6f4bff1c5489fbb6a8ae5271a6d
Eric <ericyoung111@gmail.com>
2021-10-09 21:46:50 -0600

    added timer and made the endgame function. It is called when the timer hits 0. Score goes up when the answer is right. TImer goes down when answer is wrong. Score and initials saved to local storage and the scores are pulled from localstorage when page opens.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,105 @@
-// Define DOM variables
+// Define javascript variables that access DOM elements
 const startBtn = document.getElementById("start-quiz-btn");
 var quizContent = document.getElementById("quiz-content");
 var quiz = document.getElementById("quiz");
 var timerEl = document.getElementById("timer");
+var feedBack = document.createElement("span");
+if(localStorage.getItem('scores')==="") {
+    var scores = [];
+}
+else {
+    var scores = JSON.parse(localStorage.getItem('scores'));
+}
+
+//define javascript variables
 var score = 0;
 var timer = 60;
 var currentQuestion = 0;
-var feedBack = document.createElement("span");
+
+//Starting Parameters
+timerEl.innerHTML = timer
+
+//quiz questions
 const quizQuestion = [{
-    question: "Who is my favorite person?",
+    question: "Question 1: Who is my favorite person?",
     answers: ["Anna", "Jeff", "Ron", "Russ"],
     correctAnswer: 0,
     },
 
-    {question: "Who is my favorite dog?",
+    {question: "Question 2: Who is my favorite dog?",
     answers: ["Ralph", "Nasty Neighbor Dogs", "Lilly", "Niko"],
     correctAnswer: 3,
     },
 
-    {question: "What do I like to do most with Anna?",
+    {question: "Question 3: What do I like to do most with Anna?",
     answers: ["Cuddle", "Run", "Tell her she's pretty", "Eat food"],
     correctAnswer: 0,
     },
-    
+
+    {question: "Question 4: What is my favorite food?",
+    answers: ["Burritos", "Salmon", "Lettuce", "Kale"],
+    correctAnswer: 1,
+    },
 ]
 
-//Setting starting parameters
 
 //Function for removing and adding elements upon starting the quiz
 initializeQuiz = function() {
-quizContent.removeChild(startBtn);
-quizContent.removeChild(quizContent.getElementsByTagName("p")[0]);
-quiz.appendChild(feedBack);
-quiz.getElementsByTagName("span")[0].classList.add("flex-box", "w-100", "flex-center",)
+    quizContent.removeChild(startBtn);
+    quizContent.removeChild(quizContent.getElementsByTagName("p")[0]);
+    quiz.appendChild(feedBack);
+    quiz.getElementsByTagName("span")[0].classList.add("flex-box", "w-100", "flex-center",)    
+    countdown();
 };
 
+//remove previous quiz questions
 clearQuizContent = function() {
     for(i=0; i<quizQuestion[currentQuestion].answers.length; i++) {
-        // console.log(quizContent.getElementsByTagName("button")[i])
         quizContent.removeChild(quizContent.getElementsByTagName("button")[0]);
     };
 };
 
 endGame = function () {
-    
+    //prompt tells user they are done with the quiz
+    quizContent.firstElementChild.innerText = "All done!";
+
+    //constants to build html for end of game sequence
+    const finalScore = document.createElement("span");
+    const initialsInput = document.createElement("input");
+    const requestInitials = document.createElement("label");
+    const initialsSubmit = document.createElement("button");
+
+    //attributes for input and label
+    initialsInput.setAttribute("name", "initials")
+    initialsInput.setAttribute("id", "initials")
+    requestInitials.setAttribute("for", "initials")
+
+    //text for end of game html elements
+    finalScore.innerHTML = "Your final score is: " + score;
+    requestInitials.innerHTML = "Enter initials:";
+    initialsSubmit.innerHTML = "submit";
+
+    //styles for end game html elements
+    finalScore.classList.add("flex-box", "width-100", "flex-center")
+
+    //append end of game html elements to the DOM
+    quizContent.appendChild(finalScore);
+    quizContent.appendChild(requestInitials);
+    quizContent.appendChild(initialsInput);
+    quizContent.appendChild(initialsSubmit);
+
+    //Upon submission of initials create score object with initials and associated score, push object to scores array
+    initialsSubmit.addEventListener("click", function() {
+        var playerScore = {
+            player: document.getElementById("initials").value,
+            score: score,
+        };
+        console.log(playerScore)
+        scores.push(playerScore);
+        localStorage.setItem('scores', JSON.stringify(scores));
+    })
+
+     
 }
 
 //Function that creates quiz questions and determines if answer is correct
@@ -62,27 +118,28 @@ populateQuestions = function() {
         //listen for which button is clicked
         answerButton.addEventListener('click', function() {
             if(this.id == quizQuestion[currentQuestion].correctAnswer) {
-                debugger
                 score++;
-                feedBack.innerHTML = "Correct"
+                feedBack.innerHTML = "You answered question " + (currentQuestion+1) + " correctly.";
                 if(currentQuestion<quizQuestion.length -1) {
                     currentQuestion++
                     clearQuizContent()
                     populateQuestions()
                 }
                 else {
+                    clearQuizContent()
                     endGame()
                 }
             }
             else {
                 timer -= 10;
-                feedBack.innerHTML = "Wrong"
+                feedBack.innerHTML = "You answered question " + (currentQuestion+1) + " incorrectly";
                 if(currentQuestion<quizQuestion.length -1) {
                     currentQuestion++
                     clearQuizContent()
                     populateQuestions()
                 }
                 else {
+                    clearQuizContent()
                     endGame()
                 }
             }
@@ -95,15 +152,19 @@ populateQuestions = function() {
 };
 
 //function for countdown
-
-timerEl.innerHTML = timer
+countdown = function() {
+    setInterval(function() {
+        timer--;
+        timerEl.innerHTML = timer
+        if(timer===0) {
+            clearQuizContent();
+            endGame();
+            clearInterval(countdown);
+        }
+    },
+    1000);
+};
 
 //Event listener for start quiz button
 startBtn.addEventListener("click", populateQuestions);
 startBtn.addEventListener("click", initializeQuiz);
-
-//radial buttons for quiz questions
-
-//for loop to loop over available questions
-
-//else statement for run out of questions or time
